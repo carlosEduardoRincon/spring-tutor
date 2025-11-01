d52ebfdf6829177fa3a0d388d715f35f3509ab87
carlosEduardoRincon <carloscaca147@gmail.com>
2025-11-01 16:52:54 -0300

    feat: project structure

---
+++ src/main/java/br/com/rincon/spring/TutorApplication.java
@@ -0,0 +1,13 @@
+package br.com.rincon.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorApplication.class, args);
+	}
+
+}
